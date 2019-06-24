01423dc6c8d606db72fe592c9563ec70a9f82da8
吴小雪 <1191@qq.com>
2019-06-24 18:05:26 +0800

    首页

---
+++ RNDemo/Pages/Constant.js
@@ -0,0 +1,4 @@
+import {Dimensions} from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
--- RNDemo/Pages/home/Home.js
+++ RNDemo/Pages/home/Home.js
@@ -6,7 +6,8 @@ import {
     StyleSheet,
 } from 'react-native';
 import Icon from  '../icon/IconFont'
-
+import  {SCREEN_WIDTH} from '../Constant'
+import Swiper from "../../node_modules/react-native-swiper/index";
 export default class home extends Component {
     constructor(props){
         super(props)
@@ -14,12 +15,47 @@ export default class home extends Component {
 
         }
     }
+    renderNav(){
+        return (
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                height:64,
+                backgroundColor: '#d81e06'
+            }}>
+                <View style={{
+                    width:'100%',
+                    height:64,
+                    alignItems:'center',
+                    justifyContent:'center',
+                    paddingTop:20,
+                    backgroundColor:'#d81e06'
+                }}>
+                    <Text style={{fontSize:18, color: '#FFF',fontWeight:'bold'}}>{'首页'}</Text>
+                </View>
+            </View>
+        )
+    }
+    renderSwiper(){
+        return(
+            <Swiper  autoplay height={240} showsPagination={false} loop={true}>
+                <View style={{width:'100%',height:200,backgroundColor:'yellow'}}>
+                    <Text >Hello Swiper</Text>
+                </View>
+                <View style={{width:'100%',height:200,backgroundColor:'red'}}>
+                    <Text >Beautiful</Text>
+                </View>
+                <View style={{width:'100%',height:200,backgroundColor:'blue'}}>
+                    <Text >And simple</Text>
+                </View>
+            </Swiper>
+        )
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Icon name={"right"} size={20} color={"#FFBC01"}
-                      style={{backgroundColor: 'transparent', marginTop:1}}/>
-                <Text>首页</Text>
+                {this.renderNav()}
+                {this.renderSwiper()}
             </View>
         );
     }
@@ -27,9 +63,9 @@ export default class home extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+        // flexDirection: 'column',
+        // justifyContent: 'center',
+        // alignItems: 'center',
     },
     tabBarIcon: {
         width: 21,
